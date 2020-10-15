57fc382815f47ab82cd71c1b3f8b5a1c77cfec93
Thiago Ripardo <thiago.ripardo.86@gmail.com>
2020-10-14 22:39:52 -0400

    atualizando projeto

--- model/DB/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-if(!defined("DB_NAME"))define("DB_NAME", 'estudo');
-if(!defined("DB_HOST"))define("DB_HOST", 'localhost');
-if(!defined("DB_USER"))define("DB_USER", 'root');
-if(!defined("DB_PASS"))define("DB_PASS", '');
