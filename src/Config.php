86ff5adf5e6d47312e22427c5ac3f9eea669a90a
THIAGO RIPARDO DE LIMA <tlima@sescmt.com.br>
2020-11-24 13:45:12 -0400

    Alterado config

--- src/Config.php
+++ src/Config.php
@@ -2,11 +2,11 @@
 namespace src;
 
 class Config {
-    const BASE_DIR = '/phpbo/public';
+    const BASE_DIR = '/devsbook/public';
 
     const DB_DRIVER = 'mysql';
     const DB_HOST = 'localhost';
-    const DB_DATABASE = 'phpbo';
+    const DB_DATABASE = 'devsbook';
     CONST DB_USER = 'root';
     const DB_PASS = '';
 
